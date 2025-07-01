ab6ea8e0143de96d61a9fcd12433ac75027887f4
Nikson Hernandes <Nik.Hernandes14@hotmail.com>
2025-07-01 16:44:01 -0300

    feat: mapeamento das entidades, criação das configurações do banco H2 e adicao do README

---
+++ santander-dev-week/src/main/java/me/dio/model/User.java
@@ -0,0 +1,76 @@
+package me.dio.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    //EAGER -> Carrega a entidade relacionada imediatamente (QUando chamada pelo banco de dados)
+    //LAZY -> Carrega a entidade relacionada sob demanda, quando for acessada.
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
