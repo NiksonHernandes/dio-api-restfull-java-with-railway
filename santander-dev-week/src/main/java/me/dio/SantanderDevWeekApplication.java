dbb354b449026cd2cb1d75339ed865fa9b194352
Nikson Hernandes <Nik.Hernandes14@hotmail.com>
2025-07-02 11:36:12 -0300

    feat: adicao das configuracoes de producao com o Railway, implementacao de novos servico, tratamento de excecoes e correção de bugs

--- santander-dev-week/src/main/java/me/dio/SantanderDevWeekApplication.java
+++ santander-dev-week/src/main/java/me/dio/SantanderDevWeekApplication.java
@@ -3,8 +3,13 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+@SpringBootApplication
 public class SantanderDevWeekApplication {
 
 	public static void main(String[] args) {
